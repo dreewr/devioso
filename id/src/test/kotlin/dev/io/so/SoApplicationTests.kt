c1be3dcf3f8306b0680a0a65542cd39e4fd7fcc1
André Rodrigues <falecomandresantos@gmail.com>
2023-08-07 22:07:46 -0300

    add hello world!

---
+++ id/src/test/kotlin/dev/io/so/SoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.io.so
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
